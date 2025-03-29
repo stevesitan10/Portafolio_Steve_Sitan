bf0e38a76250c55abf7e220d62385f89a12c52cb
stevesitan10 <89173518+stevesitan10@users.noreply.github.com>
2025-03-28 21:04:59 -0600

    Create Desempaquetado de tuplas.py

---
+++ python/Mutabilidad y tuplas/Desempaquetado de tuplas.py
@@ -0,0 +1,5 @@
+tupla = ("Juan", 8,"Python") # Modifica esta línea
+nombre, nota, curso = tupla
+
+print(f"{nombre} tiene {nota} en el curso de {curso}.")
+# El resultado debe ser: "Juan tiene 8 en el curso de Python."
