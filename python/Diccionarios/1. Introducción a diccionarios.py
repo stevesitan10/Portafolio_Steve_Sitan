46cbb9955cc56915d3215a3cd4b49f2c057a1bf3
stevesitan10 <89173518+stevesitan10@users.noreply.github.com>
2025-04-01 14:41:17 -0600

    Update 1. Introducción a diccionarios.py

--- python/Diccionarios/1. Introducción a diccionarios.py
+++ python/Diccionarios/1. Introducción a diccionarios.py
@@ -1,8 +1,8 @@
 #Ejercicio
-#Crea una función llamada retornar_valor que reciba como argumento un diccionario y un string llave y retorne el valor asociado a la llave en el diccionario estudiante.
+#Crea una función llamada presentar_diccionario que retorne un diccionario con las siguientes llaves y valores:
 
-#retornar_valor({"nombre": "Juan", "edad": 25}, "nombre") # Juan
-#retornar_valor({"nombre": "Juan", "edad": 25}, "edad") # 25
+#"nombre": "Juan"
+#"edad": 25
 
 def presentar_diccionario():
   estudiante = {"nombre": "Juan", "edad": 25}
