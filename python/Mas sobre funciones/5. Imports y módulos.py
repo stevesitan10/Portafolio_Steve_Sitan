98a4e70f77d2ac3d8148aadc3df990ddf82ef027
stevesitan10 <89173518+stevesitan10@users.noreply.github.com>
2025-04-01 21:50:31 -0600

    Update 5. Imports y módulos.py

--- python/Mas sobre funciones/5. Imports y módulos.py
+++ python/Mas sobre funciones/5. Imports y módulos.py
@@ -1,3 +1,9 @@
+#Ejercicio
+#Crea una función llamada calcular_raices que tome una lista de números y devuelva una lista con la raíz cuadrada de cada número. Para esto, debes importar el módulo math y usar la función sqrt().
+
+#calcular_raices([4, 9, 16, 25])  # Debe devolver: [2.0, 3.0, 4.0, 5.0]
+
+
 from math import sqrt
 
 def calcular_raices(lista):
